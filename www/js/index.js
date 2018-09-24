ade21aa1813b48a0ad0e812c79d4d7fd657eb203
aldaMarMu <you@example.com>
2018-09-25 00:18:41 +0200

    Add geolocation plugin

--- www/js/index.js
+++ www/js/index.js
@@ -13,7 +13,29 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
 		}
         
+		var onSuccess = function(position) {
+        	alert(	'Latitude: '          + position.coords.latitude          + '\n' +
+              		'Longitude: '         + position.coords.longitude         + '\n' +
+              		'Altitude: '          + position.coords.altitude          + '\n' +
+              		'Accuracy: '          + position.coords.accuracy          + '\n' +
+              		'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              		'Heading: '           + position.coords.heading           + '\n' +
+              		'Speed: '             + position.coords.speed             + '\n' +
+              		'Timestamp: '         + position.timestamp                + '\n');
+    	};
+
+    	// onError Callback receives a PositionError object
+    	//
+    	function onError(error) {
+        	alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    	}
+
 		document.getElementById('sayHello').onclick = function () { alert("hola"); }
+
+		document.getElementById('geoloc').onclick = function () { 
+			navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		}
 
 		document.getElementById('takePhoto').onclick = function () {
 					navigator.camera.getPicture( function(imageURI)
